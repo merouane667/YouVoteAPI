2cb30de6928b6d1d9bc30feed4f68918100a581a
Wail <wail.ouaissa@ynov.com>
2024-06-04 12:11:56 +0100

    Update candidateModel.js

--- src/models/candidateModel.js
+++ src/models/candidateModel.js
@@ -2,14 +2,13 @@ const mongoose = require('mongoose');
 
 // Candidate Schema
 const candidateSchema = new mongoose.Schema({
-    name: String,
-    party: String,
-    election: String,
+    name: { type: String, required: true },
+    party: { type: String, required: true },
+    election: { type: mongoose.Schema.Types.ObjectId, ref: 'Election', required: true },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
-  });
-  
+});
+
 const Candidate = mongoose.model('Candidate', candidateSchema);
-  
 
 module.exports = Candidate;
