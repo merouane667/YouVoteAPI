ef1962da0b10963f243a3aeafa74c2e29f701728
Wail <wail.ouaissa@ynov.com>
2024-06-04 12:12:33 +0100

    Update userModel.js

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,17 +1,31 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 // User Schema
 const userSchema = new mongoose.Schema({
-    firstName: String,  
-    lastName: String,
-    email: String,
-    password: String,
-    phoneNumber: String,
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
     role: { type: String, default: 'user' },
-    verificationCode: String,
+    verificationCode: { type: String, required: true },
+    verified: { type: Boolean, default: false },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
 });
+
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+userSchema.methods.comparePassword = function(candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password);
+};
 
 const User = mongoose.model('User', userSchema);
 
